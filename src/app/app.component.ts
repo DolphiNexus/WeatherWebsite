146f43a5b3e5b1dc76730efd64091b45dfcde65a
DolphiNexus <jairo73456@gmail.com>
2019-03-21 11:09:47 -0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import * as moment from "moment";
 
 @Component({
   selector: 'my-app',

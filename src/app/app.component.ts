19ad6ec8618718beaf5f111c119955dffc5e2688
DolphiNexus <jairo73456@gmail.com>
2019-03-21 10:45:35 -0700

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+  private appId: string;
+    private appCode: string;
+
+    public weather: any;
+
+    public constructor(private http: HttpClient) {
+        this.appId = "8jmIcMRZ5dDIuZqi7xDy";
+        this.appCode = "GsY8bsdL-liCTU0KexRlnQ";
+        this.weather = [];
+    }
+
+    public ngOnInit() {
+      if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            this.getWeather(position.coords);
+        });
+    } else {
+        console.error("The browser does not support geolocation...");
+    }
+     }
+
+    public getWeather(coordinates: any) {
+      this.http.jsonp("https://weather.cit.api.here.com/weather/1.0/report.json?product=forecast_7days_simple&latitude=" + coordinates.latitude + "&longitude=" + coordinates.longitude + "&app_id=" + this.appId + "&app_code=" + this.appCode, "jsonpCallback")
+        .pipe(map(result => (<any>result).dailyForecasts.forecastLocation))
+        .subscribe(result => {
+            this.weather = result.forecast;
+        }, error => {
+            console.error(error);
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        HttpClientJsonpModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
